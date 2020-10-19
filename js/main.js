b6872620fcd3dce5204910d2365b9d8d9bd9d9c4
jerumanu <emmanueljeru200@gmail.com>
2020-10-19 12:53:58 +0300

    add forms

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-$(Document).ready(function () {
+$(document).ready(function () {
     function pizaa(crust, topping, size) {
         this.crust = crust;
         this.topping = topping;
         this.size = size;
 
-        var newOder = []
-        var total =[0]
+
     };
+    var newOder = []
+    var total = 0
     pizaa.prototype.myToppingPrice = function () {
         if (this.crust === "crispy") {
             if (this.topping === "pepe") {
@@ -77,7 +78,7 @@ $(Document).ready(function () {
     pizaa.prototype.totalprice = function () {
         return this.myToppingPrice() + this.mySizePrize()
     }
-    total = newOder.totalprice();
+     //total = newOder.totalprice();
 
 
 
@@ -92,10 +93,94 @@ $(Document).ready(function () {
         newOder.push(newPizzaOder)
         $("select#crust1").val("");
         $("select#topping1").val("");
-        $ ("select#size1").val("");
-        
-        
+        $("select#size1").val("");
+        total = 0;
+
+        for (let i = 0; i < newOder.length; i++) {
+            total += newOder[i].myPizzaPrice();
+        }
+
+        $("#teble").append(
+            "<tr>" +
+            '<th scope="row">' +
+
+            newPizza.crust +
+            "  @ " +
+            newPizza.myCrustPrice() +
+            "</th>" +
+            "<td>" +
+            newPizza.toppings +
+            " @ " +
+            newPizza.myToppingsPrice() +
+            "</td>" +
+            "<td>" +
+            newPizza.Size +
+            " @ " +
+            newPizza.mySizePrice() +
+            "</td>" +
+            "<td>" +
+            newPizza.myPizzaPrice() +
+            "</td>" +
+            "</tr>"
+        );
+        $("#table").append("");
+        if (newOrder.length > 0) {
+            $("#form-title").empty();
+            $("#form-title").append("Add Another Order");
+        }
+
+        $("#Amount2").fadeIn();
+        $("#Procced").fadeIn();
+        $("#orders-div").fadeIn();
+
+        $("#Amount2").empty();
+        $("#Amount2").append(totalAmount);
+        $(".Amount2").show();
     });
-  
-   
+    $("#Procced").click(function () {
+        $(".checkout-options").show();
+    });
+
+    $("#proceed-form").submit(function (event) {
+        event.preventDefault();
+        var name = $("#name").val();
+        var deliveryOption = $("#option1").val();
+        customerName = name;
+        $("#name").val("");
+        $("#option1").val("");
+        $(".checkout-options").hide();
+        $("#Procced").hide();
+        $("#Amount2").empty();
+        if (deliveryOption === "deliver") {
+            $(".location").show();
+            $(".delivery-cost").show();
+            $("#Amount2").empty();
+            $("#delivery-amount").append(300);
+            totalAmount += 300;
+            $("#Amount2").empty();
+            $("#Amount2").empty();
+            $("#Amount2").append(totalAmount);
+            $(".Amount2").show();
+
+            $("#Amount2").append(totalAmount);
+        } else {
+            alert(customerName + ": Your total bill is Ksh. " + totalAmount + ". Your order will be ready for collection in the next one hours");
+        }
+    });
+
+    $(".location-form").submit(function (event) {
+        event.preventDefault();
+        var estateEntered = $("#estate").val();
+        var houseNumberEntered = $("#house-number").val();
+        estate = estateEntered;
+        houseNumber = houseNumberEntered;
+        $("#checkout").hide();
+        $(".location").hide();
+        $("#Amount2").empty();
+        $("#Amount2").append(totalAmount);
+        $(".Amount2").show();
+        alert(customerName + ": Your total bill is Ksh. " + totalAmount + ". Your order will be delivered to " + estate + ", " + houseNumber + " in the next one hours");
+    });
+
+
 })
